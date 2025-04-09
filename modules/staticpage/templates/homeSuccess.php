ff916aa78947058595d805b24279dcfdd94882a1
Mathieu <mathieu.deschaine@gmail.com>
2025-04-09 16:56:44 -0700

    carousel img float

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -103,42 +103,42 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide1.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide1.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>Al Monner News Negatives</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide2.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide2.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>Reuniting Finley and Bohlman</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide3.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide3.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>Kiser Photo Co. Photographs</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide4.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide4.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>Oregon Journal Photographic Negatives</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide5.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide5.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>Portland General Electric Photograph Collection</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
-      <img src="/plugins/arORB5Plugin/images/slide6.jpg" class="d-block w-100" alt="...">
+      <img src="/plugins/arORB5Plugin/images/slide6.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block bg-dark">
         <h5>OHS Maps Collection</h5>
         <p>Some representative placeholder content for the first slide.</p>
