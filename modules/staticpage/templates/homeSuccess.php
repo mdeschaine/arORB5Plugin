da638b77bbad590520dae6a88fdb3a141bc1473d
Mathieu <mathieu.deschaine@gmail.com>
2025-04-09 15:55:12 -0700

    image height 500 px

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -56,11 +56,11 @@
         </div>
     </div>
 </div>
-<div class="card mb-3">
+<div class="card mb-3" style="max-height: 500px;">
   <img src="/plugins/arORB5Plugin/images/OrgLot146_b06_SalemCapital_4.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Digitizing the Oregon Story: Creating Access to Significant Legal and Political Oral Histories</h5>
-    <p class="card-text">The Digitizing the Oregon Story project preserves and creates broad access to 212 Oral History interviews of significant legal and political figures, originally created on analog audio and video tapes and previously available only onsite in the OHS Research Library.</p>
+    <p class="card-text">Creating access to 212 Oral History interviews of significant legal and political figures, originally created on analog audio and video tapes and previously available only onsite in the OHS Research Library.</p>
     <p class="card-text"><small class="text-muted">Funded by the Institute of Museum and Library Services</small></p>
     <a href="#" class="btn btn-primary">Learn More</a>
   </div>
