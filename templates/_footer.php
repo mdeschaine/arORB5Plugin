b8b18c0317cffc7b53c5bfe400b10320c0bf3816
Mathieu <mathieu.deschaine@gmail.com>
2025-04-10 14:56:27 -0700

    footer spacing

--- templates/_footer.php
+++ templates/_footer.php
@@ -7,7 +7,7 @@
                 <p class="foot-signoff__message">The Oregon Historical Society is dedicated to making Oregon's long, rich history visible and accessible to all. For more than a century, the Oregon Historical Society has served as the state's collective memory, preserving a vast collection of artifacts, photographs, maps, manuscript materials, books, films, and oral histories. Our research library, museum, digital platform, educational programming, and historical journal make Oregon's history open and accessible to all. We exist because history is powerful, and because a history as deep and rich as Oregon's cannot be contained within a single story or point of view.</p>
             </header>
         </div>
-        <div class="row row-cols-3">
+        <div class="row row-cols-3 gy-4">
             <div class="foot-contact col-md-3">
                 <h3 class="foot__section-title">Contact Us</h3>
                 <p>Oregon Historical Society <br>1200 SW Park Ave <br>Portland, OR 97205</p>
@@ -29,7 +29,7 @@
                 <p><strong><a href="https://app.e2ma.net/app2/audience/signup/1780482/32549/" target="blank">Sign up to receive our newsletter!</a></strong></p>
             </div>
         </div>
-        <div class="row row-cols-2">
+        <div class="row row-cols-2 gy-4">
             <div class="foot-hours col-md-3">
                 <a name="hours" id="hours"></a>
                 <h3 class="foot__section-title">Library Hours</h3>
@@ -40,7 +40,7 @@
                     <p><a class="CP___PAGEID_55275" href="https://www.ohs.org/research-and-library/about-the-library/plan-your-visit/index.cfm" target="_blank">Visit the Library</a></p>
             </div>
         </div>
-        <div class="row row-cols-2">
+        <div class="row row-cols-2 gy-4">
             <div class="foot-support col-md-6">
                 <h3 class="foot__section-title">Support OHS <br>Digital Collections</h3>
                 <p>The Oregon Historical Society is a 501(c)(3) non-profit organization. Federal Tax ID 93-0391599.</p>
@@ -48,7 +48,7 @@
                 <br><a href="http://www.ohs.org/support/forward-campaign.cfm" target="_blank">FORWARD! Campaign</a></p>
             </div>
         </div>
-        <div class="row foot-footer">
+        <div class="row foot-footer gy-4">
             <p class="foot__legal"><a class="foot-footer__link" href="http://www.ohs.org/contact.cfm" target="_blank">Contact OHS</a> <a class="foot-footer__link" href="http://www.ohs.org/privacy-policy.cfm" target="_blank">Privacy Policy</a> </p>
             <p class="foot__copyright">© Copyright 2017 Oregon Historical Society</p>
         </div>
