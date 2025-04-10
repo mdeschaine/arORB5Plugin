622947161329d328782f41a17eb15d881dcab04f
Mathieu <mathieu.deschaine@gmail.com>
2025-04-10 12:10:56 -0700

    footer

---
+++ templates/_footer.php
@@ -0,0 +1,82 @@
+<footer class="foot">
+    <div class="ohsrow">
+        <header class="foot-signoff">
+            <a name="footer" id="footer"></a>
+            <h1 class="foot-signoff__title">Experience Oregon History</h1>
+            <p class="foot-signoff__message">The Oregon Historical Society is dedicated to making Oregon's long, rich history visible and accessible to all. For more than a century, the Oregon Historical Society has served as the state's collective memory, preserving a vast collection of artifacts, photographs, maps, manuscript materials, books, films, and oral histories. Our research library, museum, digital platform, educational programming, and historical journal make Oregon's history open and accessible to all. We exist because history is powerful, and because a history as deep and rich as Oregon's cannot be contained within a single story or point of view.</p>
+        </header>
+    </div>
+    <div class="ohsrow">
+        <div class="foot__left-side">
+            <div class="foot-contact">
+                <h3 class="foot__section-title">Contact Us</h3>
+                <p>Oregon Historical Society <br>1200 SW Park Ave <br>Portland, OR 97205</p>
+                <p><a href="tel:+15032221741">503.306.5240</a><br> <a href="mailto:libreference@ohs.org ">libreference@ohs.org</a><br> <a href="http://www.ohs.org/contact.cfm" target="_blank">Staff Directory</a></p>
+            </div>
+            <div class="foot-resources">
+                <h3 class="foot__section-title">Resources</h3>
+                <ul>
+                    <li><?php echo link_to(__('About'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
+                    <li><?php echo link_to(__('Copyright'), array('module' => 'staticpage', 'slug' => 'copyright-2')) ?></li>
+                    <li><?php echo link_to(__('Terms of Service'), array('module' => 'staticpage', 'slug' => 'terms')) ?></li>
+                    <li><?php echo link_to(__('Contact Us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+                    <li><?php echo link_to(__('Search Help'), array('module' => 'staticpage', 'slug' => 'help-search')) ?></li>
+                </ul>
+            </div>
+            <div class="foot-hours">
+                <a name="hours" id="hours"></a>
+                <h3 class="foot__section-title">Library Hours</h3>
+                <p><span class="foot-hours__times">Tuesdays:
+                    <br>1pm – 5pm
+                    <br>Wednesday – Saturday:
+                    <br>10am – 5pm</span></p>
+                <p><a class="CP___PAGEID_55275" href="https://www.ohs.org/research-and-library/visit-the-library/index.cfm" target="_blank">Visit the Library</a></p>
+            </div>
+            <div class="foot-support">
+                <h3 class="foot__section-title">Support OHS <br>Digital Collections</h3>
+                <p>The Oregon Historical Society is a 501(c)(3) non-profit organization. Federal Tax ID 93-0391599.</p>
+                <p><a href="https://orhs.ejoinme.org/digitalcollections" target="_blank">Donate Online</a>
+                    <br><a href="http://www.ohs.org/support/forward-campaign.cfm" target="_blank">FORWARD! Campaign</a></p>
+            </div>
+        </div>
+        <div class="foot__right-side">
+            <div class="foot-newsletter">
+                <h3 class="foot__section-title">Newsletter</h3>
+                <p>Want to keep up with the latest news from The Oregon Historical Society?</p>
+                <p><strong><a href="https://app.e2ma.net/app2/audience/signup/1780482/32549/" target="blank">Sign up to receive our newsletter!</a></strong></p>
+            </div>
+            <div class="foot-social">
+                <h3 class="foot__section-title">Follow Us</h3>
+                <ul class="social-icons">
+                    <li class="social-icons__item"><a class="social-icons__link" title="Facebook" href="https://www.facebook.com/oregonhistory" target="_blank"><svg viewBox="0 0 100 100" class="icon-fbook">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-fbook"></use>
+                            </svg></a><span class="screen-reader">Facebook</span></li>
+                    <li class="social-icons__item"><a class="social-icons__link" title="Twitter" href="https://twitter.com/orhist" target="_blank"><svg viewBox="0 0 100 100" class="icon-twitter">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use>
+                            </svg></a><span class="screen-reader">Twitter</span></li>
+                    <li class="social-icons__item"><a class="social-icons__link" title="Instagram" href="https://www.instagram.com/oregonhistoricalsociety/?hl=en" target="_blank"><svg viewBox="0 0 100 100" class="icon-pinterest">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-instagram"></use>
+                            </svg></a><span class="screen-reader">Instagram</span></li>
+                    <li class="social-icons__item"><a class="social-icons__link" title="YouTube" href="http://www.youtube.com/channel/UCeCtHcPsdEys360N-qyPUPA" target="_blank"><svg viewBox="0 0 100 100" class="icon-youtube">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-youtube"></use>
+                            </svg></a><span class="screen-reader">YouTube</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="ohsrow foot-footer">
+        <p class="foot__legal"><a class="foot-footer__link" href="http://www.ohs.org/contact.cfm" target="_blank">Contact OHS</a> <a class="foot-footer__link" href="http://www.ohs.org/privacy-policy.cfm" target="_blank">Privacy Policy</a> </p>
+        <p class="foot__copyright">© Copyright 2017 Oregon Historical Society</p>
+    </div>
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BK4MXDS94K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BK4MXDS94K');
+</script>
+
+</footer>
