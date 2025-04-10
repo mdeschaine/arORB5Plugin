f0f2f76200fcb3c3bdc034c2b5ee020136fa1b2d
Mathieu <mathieu.deschaine@gmail.com>
2025-04-10 14:30:50 -0700

    footer

--- templates/_footer.php
+++ templates/_footer.php
@@ -7,7 +7,7 @@
                 <p class="foot-signoff__message">The Oregon Historical Society is dedicated to making Oregon's long, rich history visible and accessible to all. For more than a century, the Oregon Historical Society has served as the state's collective memory, preserving a vast collection of artifacts, photographs, maps, manuscript materials, books, films, and oral histories. Our research library, museum, digital platform, educational programming, and historical journal make Oregon's history open and accessible to all. We exist because history is powerful, and because a history as deep and rich as Oregon's cannot be contained within a single story or point of view.</p>
             </header>
         </div>
-        <div class="row row-cols-3">
+         <div class="row row-cols-3">
                 <div class="foot-contact col-md-3">
                     <h3 class="foot__section-title">Contact Us</h3>
                     <p>Oregon Historical Society <br>1200 SW Park Ave <br>Portland, OR 97205</p>
@@ -28,26 +28,24 @@
                     <p>Want to keep up with the latest news from The Oregon Historical Society?</p>
                     <p><strong><a href="https://app.e2ma.net/app2/audience/signup/1780482/32549/" target="blank">Sign up to receive our newsletter!</a></strong></p>
                 </div>
-        </div>
-            <div class="row">
-                <div class="foot-hours">
-                    <a name="hours" id="hours"></a>
-                    <h3 class="foot__section-title">Library Hours</h3>
-                    <p><span class="foot-hours__times">Tuesdays:
-                        <br>1pm – 5pm
-                        <br>Wednesday – Saturday:
-                        <br>10am – 5pm</span></p>
-                    <p><a class="CP___PAGEID_55275" href="https://www.ohs.org/research-and-library/about-the-library/plan-your-visit/index.cfm" target="_blank">Visit the Library</a></p>
-                </div>
-                <div class="foot-support">
-                    <h3 class="foot__section-title">Support OHS <br>Digital Collections</h3>
-                    <p>The Oregon Historical Society is a 501(c)(3) non-profit organization. Federal Tax ID 93-0391599.</p>
-                    <p><a href="https://orhs.ejoinme.org/digitalcollections" target="_blank">Donate Online</a>
-                        <br><a href="http://www.ohs.org/support/forward-campaign.cfm" target="_blank">FORWARD! Campaign</a></p>
-                </div>
             </div>
-            
-        </div>
+                <div class="row row-cols-2">
+                    <div class="foot-hours col-md-3">
+                        <a name="hours" id="hours"></a>
+                        <h3 class="foot__section-title">Library Hours</h3>
+                        <p><span class="foot-hours__times">Tuesdays:
+                            <br>1pm – 5pm
+                            <br>Wednesday – Saturday:
+                            <br>10am – 5pm</span></p>
+                        <p><a class="CP___PAGEID_55275" href="https://www.ohs.org/research-and-library/about-the-library/plan-your-visit/index.cfm" target="_blank">Visit the Library</a></p>
+                    </div>
+                    <div class="foot-support col-md-9">
+                        <h3 class="foot__section-title">Support OHS <br>Digital Collections</h3>
+                        <p>The Oregon Historical Society is a 501(c)(3) non-profit organization. Federal Tax ID 93-0391599.</p>
+                        <p><a href="https://orhs.ejoinme.org/digitalcollections" target="_blank">Donate Online</a>
+                            <br><a href="http://www.ohs.org/support/forward-campaign.cfm" target="_blank">FORWARD! Campaign</a></p>
+                    </div>
+                </div>
         <div class="row foot-footer">
             <p class="foot__legal"><a class="foot-footer__link" href="http://www.ohs.org/contact.cfm" target="_blank">Contact OHS</a> <a class="foot-footer__link" href="http://www.ohs.org/privacy-policy.cfm" target="_blank">Privacy Policy</a> </p>
             <p class="foot__copyright">© Copyright 2017 Oregon Historical Society</p>
