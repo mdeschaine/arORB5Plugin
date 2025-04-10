86043bdb4902aa68b12f111b254884a2c2657e5e
Mathieu <mathieu.deschaine@gmail.com>
2025-04-09 17:14:53 -0700

    change carousel breakpoints

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -104,49 +104,49 @@
   <div class="carousel-inner">
     <div class="carousel-item active"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide1.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>Al Monner News Negatives</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide2.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>Reuniting Finley and Bohlman</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide3.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>Kiser Photo Co. Photographs</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide4.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>Oregon Journal Photographic Negatives</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide5.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>Portland General Electric Photograph Collection</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item"  data-bs-interval="10000">
       <img src="/plugins/arORB5Plugin/images/slide6.jpg" class="d-block w-100 img-fluid" alt="...">
-      <div class="carousel-caption d-none d-md-block bg-dark">
+      <div class="carousel-caption d-none d-sm-block bg-dark">
         <h5>OHS Maps Collection</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="carousel-control-prev-icon"></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
