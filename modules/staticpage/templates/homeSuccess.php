d5ccd4102e370bb808f9ac577405eb3adbb16841
Mathieu <mathieu.deschaine@gmail.com>
2025-04-10 11:21:25 -0700

    add images

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -1,23 +1,24 @@
 
-
-<?php echo get_component('menu', 'staticPagesMenu'); ?>
-<?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID); ?>
-  <?php if ($browseMenu->hasChildren()) { ?>
-    <section class="card mb-3">
-      <h2 class="h5 p-3 mb-0">
-        <?php echo __('Browse by'); ?>
-      </h2>
-      <div class="list-group list-group-flush">
-        <?php foreach ($browseMenu->getChildren() as $item) { ?>
-          <a
-            class="list-group-item list-group-item-action"
-            href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>">
-            <?php echo esc_specialchars($item->getLabel(['cultureFallback' => true])); ?>
-          </a>
+<div id="homepage-hero" class="row" title="...">
+    <div class="container">
+            <?php echo get_component('menu', 'staticPagesMenu'); ?>
+        <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID); ?>
+        <?php if ($browseMenu->hasChildren()) { ?>
+            <h2 class="h5 p-3 mb-0">
+                <?php echo __('Browse by'); ?>
+            </h2>
+            <div class="list-group list-group-flush">
+                <?php foreach ($browseMenu->getChildren() as $item) { ?>
+                <a
+                    class="list-group-item list-group-item-action"
+                    href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>">
+                    <?php echo esc_specialchars($item->getLabel(['cultureFallback' => true])); ?>
+                </a>
+                <?php } ?>
+            </div>
         <?php } ?>
-      </div>
-    </section>
-  <?php } ?>
+    </div>
+  </div>
 
 <div class="container" style="max-height: 500px;">
     <div class="row">
